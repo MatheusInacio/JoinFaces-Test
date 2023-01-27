1cfc7a6cd7024a85991e42f374bca6b2d190652b
Matheus Inácio <matheus.inacio@outlook.com>
2023-01-27 10:20:13 -0300

    Initial Commit

---
+++ src/main/java/br/com/joinfacestest/joinfacestest/JoinfacesTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.joinfacestest.joinfacestest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinfacesTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinfacesTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joinfacestest/joinfacestest/JoinfacesTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joinfacestest.joinfacestest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinfacesTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
